8fa004ab2d54741a5abaab49252fc599b9145141
ogtsyba <oleg.tsyba.ua@gmail.com>
2025-08-24 22:11:42 +0300

    hasura added

---
+++ hasura-weather/migrations/default/1756062432983_create_identity_table/down.sql
@@ -0,0 +1,7 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- create table identity (
+--     id uuid primary key,
+--     email varchar(64),
+--     name varchar(64)
+-- );
---
+++ hasura-weather/migrations/default/1756062432983_create_identity_table/up.sql
@@ -0,0 +1,5 @@
+create table identity (
+    id uuid primary key,
+    email varchar(64),
+    name varchar(64)
+);
